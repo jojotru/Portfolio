af10841ad67e01ad539ca0613cf32c92f1132c6d
RootBeer8 <109646119+RootBeer8@users.noreply.github.com>
2023-11-03 18:49:44 -0400

    Update 11/3

--- app.js
+++ app.js
@@ -8,10 +8,33 @@ function PageTransitions(){
     for (let i = 0; i < sectBtn.length; i++){
         sectBtn[i].addEventListener('click', function(){
             let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-            this.className += 'active-btn' ;
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn',' ');
+            this.className += ' active-btn';
         })
     }
+
+    //Sections Active Class
+    allSections.addEventListener('click', (e) => {
+        const id = e.target.dataset.id;
+        if(id){
+            //remove selected from the other btns
+            sectBtns.forEach((btn) => {
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            //hide other sections
+            sections.forEach((section)=>{
+                section.classList.remove('active')
+            })
+
+            //get id in the doc and store it in element
+            const element = document.getElementById(id) 
+            element.classList.add('active');
+
+
+        }
+    })
 }
 
 PageTransitions();
